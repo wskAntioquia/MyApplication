d1a8d03685332ec881c863519e27f3268dd0b3c3
RodrigoAGuspian <37385496+RodrigoAGuspian@users.noreply.github.com>
2018-08-31 07:54:39 -0500

    commit yus

--- app/src/main/java/com/example/worldskills/myapplication/MainActivity.java
+++ app/src/main/java/com/example/worldskills/myapplication/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+
+
 }
